7f29ae1c5efae3d0c9e9d23ea72dbc7bce2c8b1d
Ankita Garg <44531331+ankita462@users.noreply.github.com>
2020-07-01 17:51:40 +0530

    Create . Middle of the Linked List.cpp
    Given a non-empty, singly linked list with head node head, return a middle node of linked list.
    
    If there are two middle nodes, return the second middle node.
    
     
    
    Example 1:
    
    Input: [1,2,3,4,5]
    Output: Node 3 from this list (Serialization: [3,4,5])
    The returned node has value 3.  (The judge's serialization of this node is [3,4,5]).
    Note that we returned a ListNode object ans, such that:
    ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, and ans.next.next.next = NULL.
    Example 2:
    
    Input: [1,2,3,4,5,6]
    Output: Node 4 from this list (Serialization: [4,5,6])
    Since the list has two middle nodes with values 3 and 4, we return the second one.
     
    
    Note:
    
    The number of nodes in the given list will be between 1 and 100.

---
+++ . Middle of the Linked List.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        int len=0;
+        ListNode* temp=head;
+        while(temp!=NULL) {
+            len++;
+            temp=temp->next;
+        }
+        int c=0;
+        while(c<len/2) {
+            head=head->next;
+            c++;
+        }
+        return head;
+    }
+};
