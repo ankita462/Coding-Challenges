ba19f86ff276763e27f53106207bc9e7eadaac8c
Ankita Garg <44531331+ankita462@users.noreply.github.com>
2020-07-01 23:22:14 +0530

    Update Last Stone Weight.cpp

--- Last Stone Weight.cpp
+++ Last Stone Weight.cpp
@@ -10,3 +10,27 @@ public:
         return stones[0];
     }
 };
+
+# 2nd Approach
+
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq;
+        for(int i=0;i<stones.size();i++){
+            pq.push(stones[i]);
+        }
+        int a,b;
+        while(pq.size()>=2){
+            a=pq.top();
+            pq.pop();
+            b=pq.top();
+            pq.pop();
+            if(abs(a-b)>0)
+                pq.push(abs(a-b));
+        }
+        if(pq.empty())
+            return 0;
+        return pq.top();
+    }
+};
